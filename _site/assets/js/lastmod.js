a04b258446183aac95a3d9cf928081c1bc259236
jeffsolo <jeff@jeffreysolo.com>
2019-01-13 16:03:35 +0100

    tabs to spaces

--- _site/assets/js/lastmod.js
+++ _site/assets/js/lastmod.js
@@ -1,10 +1,10 @@
 window.onload = function(){
-	lastModified('modifiedOn');
+    lastModified('modifiedOn');
 };
 
 function lastModified(id){
-	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	var d = new Date(document.lastModified);
-	var sModified = [months[d.getMonth()], d.getDate() + ',' , d.getFullYear()].join(' ');
-	var inner = document.getElementById(id).innerHTML += sModified
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var d = new Date(document.lastModified);
+    var sModified = [months[d.getMonth()], d.getDate() + ',' , d.getFullYear()].join(' ');
+    var inner = document.getElementById(id).innerHTML += sModified
 };
